29f826a855931ee305b844c3b89107858c6d9d9f
chulchoice <chulchoicezee@gmail.com>
2015-06-29 17:15:59 +0900

    first11

--- app/src/main/java/com/android/tests/activities/O_HttpTestActivity.java
+++ app/src/main/java/com/android/tests/activities/O_HttpTestActivity.java
@@ -9,7 +9,9 @@ import android.widget.Toast;
 
 import com.android.tests.R;
 
+import java.io.IOException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class O_HttpTestActivity extends Activity{
@@ -29,13 +31,21 @@ public class O_HttpTestActivity extends Activity{
 	public void onButtonClicked1(View view) {
 		// TODO Auto-generated method stub
 		Toast.makeText(this, "clicked1", Toast.LENGTH_SHORT).show();
-		AsyncTask<String, Void, String> execute = new AsyncTask<String, Void, String>() {
+		new AsyncTask<String, Void, String>() {
 			@Override
 			protected String doInBackground(String... params) {
-				URL url = new URL(params[0]);
-				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                URL url = null;
+                try {
+                    url = new URL(params[0]);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-				return null;
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return null;
 			}
 		}.execute(strUrl);
 	}
